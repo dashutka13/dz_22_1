13d9de11c93a53f37b711e7e8c73cbf82ce60099
dashuta <it.dashutka@gmail.com>
2024-02-07 12:48:37 +0300

    add task1

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Blog
+
 
 # admin.site.register(Category)
 # admin.site.register(Product)
@@ -16,3 +17,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'unit_price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'blog_title', 'body',)
+    list_filter = ('blog_title',)
+    search_fields = ('blog_title', 'body',)
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import product, catalog, home_page
+from catalog.views import home_page, PokemonListView, PokemonDetailView, BlogListView, BlogCreateView, BlogUpdateView, \
+    BlogDeleteView, BlogDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('product/<int:pk>/', product, name='product'),
-    path('catalog/', catalog, name='catalog')
+    # path('product/<int:pk>/', product, name='product'),
+    path('product/<int:pk>/', PokemonDetailView.as_view(), name='view_product'),
+    # path('catalog/', catalog, name='catalog')
+    path('catalog/', PokemonListView.as_view(), name='catalog'),
+    path('blog/', BlogListView.as_view(), name='blog'),
+    path('create/', BlogCreateView.as_view(), name='create_blog'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='view_blog'),
+    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog'),
 ]
