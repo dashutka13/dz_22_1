19b284c8535ed1efd6419171c12fb3eaffc31355
dashuta <it.dashutka@gmail.com>
2024-02-07 14:12:39 +0300

    add task3

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.text import slugify
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product, Blog
@@ -20,21 +21,49 @@ class PokemonDetailView(DetailView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('blog_title', 'preview', 'body')
     success_url = reverse_lazy('catalog:blog')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('blog_title', 'preview', 'body')
-    success_url = reverse_lazy('catalog:blog')
+    # success_url = reverse_lazy('catalog:blog')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('catalog:blog', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
